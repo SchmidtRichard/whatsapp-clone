2e07571544984806345368d9a82a0295bc8e46a8
SchmidtRichard <ri.schmidta@gmail.com>
2021-10-11 16:56:52 +0100

    Working On Chat Component

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,10 +1,84 @@
 import React from 'react';
 import "./Chat.css"
+import {Avatar, IconButton} from "@material-ui/core";
+import {AttachFile, InsertEmoticon, MoreVert, SearchOutlined, Mic} from "@material-ui/icons";
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>I am the chat section</h1>
+
+            <div className="chat__header">
+
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                        <AttachFile />
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+
+            </div>
+
+            <div className="chat__body">
+                <p className="chat__message">
+
+                    <span className="chat__name">Richard</span>
+                    Hello there
+
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+
+                </p>
+
+                <p className="chat__message chat__receiver">
+
+                    <span className="chat__name">Andressa</span>
+                    Oi
+
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+
+                </p>
+
+                <p className="chat__message">
+
+                    <span className="chat__name">Richard</span>
+                    What's the plans for tonight?
+
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+
+                </p>
+
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticon />
+
+                <form>
+                    <input
+                        placeholder="Type a message"
+                        type="text"
+                    />
+                    <button type="submit">Send a message</button>
+                </form>
+
+                <Mic />
+
+            </div>
+
         </div>
     );
 }
